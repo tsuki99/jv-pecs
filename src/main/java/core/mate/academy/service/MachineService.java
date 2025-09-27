35d3d17ac75d8e2cc838405b725d5a4a897a8a8f
Vlad Rublenko <vladik.rar@ukr.net>
2025-09-27 19:23:04 +0300

    fixed

--- src/main/java/core/mate/academy/service/MachineService.java
+++ src/main/java/core/mate/academy/service/MachineService.java
@@ -4,9 +4,9 @@ import core.mate.academy.model.Machine;
 import java.util.List;
 
 public interface MachineService<T extends Machine> {
-    List<Machine> getAll(Class<? extends Machine> type);
+    List<T> getAll(Class<? extends Machine> type);
 
-    void fill(List<? super Machine> machines, Machine value);
+    void fill(List<? super T> machines, T value);
 
     void startWorking(List<? extends T> machines);
 }
